4a50790c03967cfe315c2c051c6b77b5e5a69455
Yamanaka78 <jjbdumujpgm@gmail.com>
2023-07-02 22:31:08 +0900

    一覧画面の作成

--- src/companies/index.php
+++ src/companies/index.php
@@ -1,13 +1,2 @@
 <?php
-?>
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>会社情報一覧</title>
-</head>
-<body>
-  <h1>会社情報一覧</h1>
-</body>
-</html>
+include 'views/index.php';
---
+++ src/companies/views/index.php
@@ -0,0 +1,19 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>会社情報一覧</title>
+</head>
+<body>
+  <h1>会社情報一覧</h1>
+  <a href="new.php">会社情報を登録する</a>
+  <main>
+    <section>
+      <h2>株式会社aaaa</h2>
+    </section>
+  </main>
+</body>
+</html>
