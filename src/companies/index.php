52a6909b3f3aef9677c2bc192d0b386b2d33e875
Yamanaka78 <jjbdumujpgm@gmail.com>
2023-07-04 18:01:08 +0900

    xss対策処理追加

--- src/companies/index.php
+++ src/companies/index.php
@@ -1,4 +1,23 @@
 <?php
+require __DIR__ . '/lib/escape.php';
+require __DIR__ . '/lib/mysqli.php';
+function ListCompanies($link)
+{
+  $companies = [];
+  $sql ='SELECT name, establishment_date, founder FROM companies';
+  $result = mysqli_query($link, $sql);
+  while ($company = mysqli_fetch_assoc($result)){
+    $companies[] = $company;
+  }
+  mysqli_free_result($result);
+
+  return $companies;
+}
+
+$link = dbConnect();
+$companies = ListCompanies($link);
+
+
 $title = '会社情報一覧';
 $content = __DIR__ . '/views/index.php';
 include  __DIR__ . '/views/layout.php';
--- src/companies/views/index.php
+++ src/companies/views/index.php
@@ -1,8 +1,20 @@
 <h1>会社情報一覧</h1>
 <a href="new.php">会社情報を登録する</a>
 <main>
-  <section>
-    <h2>株式会社aaaa</h2>
-    <div>創業：2023 | 代表：山田直哉</div>
-  </section>
+  <?php if (count($companies) > 0) : ?>
+
+    <?php foreach ($companies as $company) : ?>
+      <section>
+        <h2>
+          <?php echo escape($company['name']); ?>
+        </h2>
+        <div>
+          創業：<?php echo escape($company['establishment_date']); ?>&nbsp;|&nbsp;代表：<?php echo escape($company['founder']) ?>
+        </div>
+      </section>
+
+    <?php endforeach; ?>
+  <?php else : ?>
+    <p>会社情報が表示されていません。</p>
+  <?php endif; ?>
 </main>
